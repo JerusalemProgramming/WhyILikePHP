64d6768d981a8937bfad9f8bde2cc4a72b13d31f
JerusalemProgramming.com <jerusalemprogramming@gmail.com>
2019-07-26 13:23:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php /* <!--  BEGIN COMMENTS --> */  ?>
+<?php /* <!--  BEGIN COMMENTS --> */  ?>
+<?php /* <!--  BEGIN COMMENTS --> */  ?>
+<?php /* <!--  BEGIN COMMENTS --> */  ?>
+
+<?php /* <!--  COMMENTS ARE HIDDEN FROM USER IN <!-- HTML COMMENTS --> THAT ARE EMBEDDED WITHIN PHP SERVER COMMENTS THAT ARE NEVER RENDERED TO USER --> */  ?>
+<?php /* <!--  THE WEB SITE LOOK AND PROOF-OF-CONCEPT FUNCTIONALITY IS MOCKED UP IN HTML5/CSS/JAVASCRIPT --> */  ?>
+<?php /* <!--  THEN THE ENTIRE WEB SITE IS DIVIDED AND COMPARTMENTALIZED INTO COMPONENTS TO BE PLACED WITHIN PHP SERVER INCLUDES --> */  ?>
+<?php /* <!--  FUNCTIONALITIES (BOTH DYNAMIC AND STATIC) ARE DEVELOPED AND PLACED WITHIN SEPARATE COMPONENTS AND CONNECTED TO THIS ONE MAIN INDEX.PHP PAGE --> */  ?>
+
 <?php include 'incl_component_webpage_head.php'; ?>
 <?php include 'incl_component_webpage_navbar.php'; ?>
 <?php include 'incl_component_webpage_banner1.php'; ?>
@@ -17,3 +27,16 @@
 <?php include 'incl_component_webpage_footer.php'; ?>
 <?php include 'incl_component_webpage_scripts.php'; ?>
 <?php include 'incl_component_webpage_end.php'; ?>
+
+<?php /* <!--  PHP IS IDEAL SOLUTION FOR RAPID WEB SITE APP DEVELOPMENT  --> */  ?>
+<?php /* <!--  PHP LOCAL DEVELOPMENT SERVER IS SIMPLE TO LAUNCH WITHIN ANY FOLDER ON YOUR COMPUTER --> */  ?>
+<?php /* <!--  INSTALL PHP, AND THEN WITHIN THAT PHP FOLDER:  OPEN A COMMAND WINDOW AND RUN THE FOLLOWING COMMAND IN THE COMMAND LINE INTERFACE (CLI) WITH YOUR SPECIFIC FOLDER THAT YOU WOULD LIKE TO BE YOUR ROOT WEB SITE DEVELOPMENT PHP WEB SERVER TEST FOLDER: --> */  ?>
+<?php /* <!--  php -S localhost:8000 -t C:\WEBSITES\TorahBibleCodes --> */  ?>
+<?php /* <!--  GO TO:  localhost:8000 IN YOUR WEB BROWSER --> */  ?>
+<?php /* <!--  YOUR PHP FILES DEVELOPMENT WORK ARE VIEWABLE ON YOUR LOCAL COMPUTER HERE:  localhost:8000 --> */  ?>
+
+<?php /* <!--  END COMMENTS --> */  ?>
+<?php /* <!--  END COMMENTS --> */  ?>
+<?php /* <!--  END COMMENTS --> */  ?>
+<?php /* <!--  END COMMENTS --> */  ?>
+
